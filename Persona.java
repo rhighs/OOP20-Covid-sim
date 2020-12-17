64c18612cc8e1488d9abc4000a2b3fc74006ca82
chrisisnotarudeguy <christian.ricci2000@gmail.com>
2020-12-17 16:32:00 +0100

    Add a rudimentary class Persona

---
+++ Persona.java
@@ -0,0 +1,8 @@
+// package it.unibo.oop.project.juri.sku.rez.savi.Persona
+
+interface Persona {
+    void    move(long x, long y, long z);
+    void    infect();
+    boolean isInfected();
+}
+
---
+++ PersonaImpl.java
@@ -0,0 +1,31 @@
+// package it.unibo.oop.project.Json.Rob.doge.savi.Persona
+
+class PersonaImpl implements Persona {
+    private long x;
+    private long y;
+    private long z;
+    private boolean infected;
+
+    static enum Protection {
+        MASK, NONE
+    }
+
+    private int id;   // suggested by juri
+    private int yolo; // suggested by savini
+
+    // graphics information, such as mesh, models, whatever
+
+    public void move(long x, long y, long z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public void infect() {
+        infected = true;
+    }
+
+    public boolean isInfected() {
+        return infected;
+    }
+}
