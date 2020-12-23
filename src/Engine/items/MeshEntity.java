4f40ba5e0cb497892c5a138999fef54832ded01a
chrisisnotarudeguy <christian.ricci2000@gmail.com>
2020-12-23 16:53:14 +0100

    ugh

---
+++ src/Engine/items/MeshEntity.java
@@ -0,0 +1,18 @@
+package Engine.items;
+
+import com.jme3.material.Material;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Mesh;
+
+public class MeshEntity extends Entity {
+    private Mesh mesh;
+
+    public MeshEntity(final int id, final String name, final Mesh mesh, final Material material) {
+        super(id, name, new Geometry(name, mesh), material);
+        this.mesh = mesh;
+    }
+
+    public Mesh getMesh() {
+        return mesh;
+    }
+}
