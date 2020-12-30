2609d7ab6ae6db694ea66cc96e516f5663ff49c5
juri simoncini <juri.simoncini@studio.unibo.it>
2020-12-30 14:42:49 +0100

    update infection algo

--- src/Simulation/InfectionImpl.java
+++ src/Simulation/InfectionImpl.java
@@ -7,19 +7,27 @@ import java.util.Random;
  * @author simon
  */
 class InfectionImpl implements InfectionAlg{
-
+    //vedere uso
     private static int INF_RADIUS = 5;
     @Override
     public void infection(Person p) {
-        if(!p.isInfected()  && p.getMask().getStatus() == Mask.MaskStatus.DOWN){
-            p.infect();
-        }else{
-            maskInfection(p);
+        if(!p.isInfected()){
+            int param = checkTrasmissibility(p);
+            if(param == 100){
+                p.infect();
+            }else{
+                // se trasm piu di 60 generi numero random da 0 a 100 e se num > complementare, infetti
+                //else se trasm meno di 60% generi numero random da 0 a 100 e se num > complementare, infetti
+                    Random rand = new Random(); //instance of random class
+                    final int upperbound = 101;
+                    if(rand.nextInt(upperbound) >= (100 - param)){
+                        p.infect();
+                    }
+                }
+            }
         }
-        
-    }
     //infection depends from Mask status
-    private void maskInfection(Person p){
+    /*private void maskInfection(Person p){
         //bad
             Random rand = new Random(); //instance of random class
             final int upperbound = 101;
@@ -36,19 +44,17 @@ class InfectionImpl implements InfectionAlg{
                     p.infect();
                 }
             }
-    }
-    
-    private void checkTrasmissibility(Person p){
+    }*/
+    // mask status 70% mask type 30%
+    private int checkTrasmissibility(Person p){
+        //percentage of trasmissibility
         int perc = 0;
-        //check maskStatus refactor like bottom
-        /*if(p.getMask().getStatus() == Mask.MaskStatus.DOWN){
-            perc += 5;
-        }else{
-            perc += 2;
-        }*/
-        perc += 5 - p.getMask().getStatus().ordinal();
-        //check maskProtection (if FFP1 perc is 50)
-        perc += 5 - p.getMask().getProtection().ordinal();
+        //Migliorabile
+        //check maskStatus (if Down is 70%)
+        perc += 70 - (70 * p.getMask().getProtection().ordinal() / 2);
+        //check maskProtection (if FFP1 perc is 30%)        
+        perc += 30 - 10 * (p.getMask().getProtection().ordinal());
+        return perc;
     }
     //calcolare indice di trasmissibilita attraverso parametri : mask, protMask, MaskStat from 0 to 10
     //es: se mascherina down e FFP1 = 100%
