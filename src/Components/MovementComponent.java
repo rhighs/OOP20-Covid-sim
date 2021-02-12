c2ea93be3fad2b5bf7b50f20aef584b72d9af05f
chrisisnotarudeguy <christian.ricci2000@gmail.com>
2021-02-12 23:45:29 +0100

    Update movement component with better state management

--- src/Components/MovementComponent.java
+++ src/Components/MovementComponent.java
@@ -4,20 +4,72 @@ import com.jme3.scene.Spatial;
 
 public class MovementComponent {
     private Spatial scene;
-    private PathGenerator pg;
-    private PathFollower pathFollower;
+    private Spatial spatial;
+    private BetterCharacterControl spatialControl;
+    private PathGenerator pathGen;
+    private Waypoint currPoint;
+    private List<Waypoint> wayPoints = new ArrayList<>();
+
+    private enum State {
+        NO_MORE_WAYPOINTS,
+        FOLLOW_WAYPOINT,
+        AT_WAYPOINT,
+    }
+    State state;
 
     public MovementComponent(final Spatial spatial, final Spatial scene) {
         this.scene = scene;
-        pg = new PathGenerator(scene);
-        pathFollower = new PathFollower(spatial, pg);
+        this.spatial = spatial;
+        this.spatialControl = spatial.getControl(BetterCharacterControl.class);
+        this.pathGen = new PathGenerator(scene);
     }
 
-    public void startPathFollower(){
-        // pathFollower.start();
+    private void finishedWaypoints() {
+        // create a new path
+        Vector3f target = pathGen.getRandomPoint();
+        wayPoints = pathGen.getPath(spatial.getLocalTranslation(), target);
+        try {
+            currPoint = wayPoints.remove(wayPoints.size() - 1);
+        } catch (UnsupportedOperationException e) {
+            System.out.println("caught an exception here");
+            System.out.println("no wayPoints: " + wayPoints.size() + ", target: " + target.getX() + "," + target.getY() + "," + target.getZ());
+            System.exit(1);
+        }
+        System.out.println(", target: " + target.getX() + "," + target.getY() + "," + target.getZ());
     }
 
-    public void update(float tpf){
-        pathFollower.update();
+    private void followWaypoint() {
+        Vector3f currPointVector = currPoint.getPosition();
+        if (spatial.getLocalTranslation().distance(currPointVector) <= 1) {
+            // change state
+            this.spatialControl.setWalkDirection(Vector3f.ZERO);
+            this.state = State.AT_WAYPOINT;
+        } else {
+            // continue getting nearer
+            Vector3f v = currPointVector.subtract(spatial.getLocalTranslation());
+            spatialControl.setWalkDirection(v.normalize().mult(8));
+            spatialControl.setViewDirection(v.negate());
+        }
+    }
+
+    private void atWaypoint() {
+        if (wayPoints.isEmpty()) {
+            this.state = State.NO_MORE_WAYPOINTS;
+        } else {
+            this.currPoint = wayPoints.remove(wayPoints.size() - 1);
+            this.state = State.FOLLOW_WAYPOINT;
+        }
+    }
+
+    public void update(float tpf) {
+        switch (state) {
+        case NO_MORE_WAYPOINTS: finishedWaypoints(); break;
+        case FOLLOW_WAYPOINT:   followWaypoint();    break;
+        case AT_WAYPOINT:       atWaypoint();        break;
+        }
+        // if (currPoint == null) {
+        //     System.out.println("currPoint is null");
+        //     System.exit(1);
+        // }
     }
 }
--- src/Components/PathFollower.java
+++ src/Components/PathFollower.java
@@ -8,44 +8,13 @@ import com.jme3.math.Vector3f;
 import com.jme3.scene.Spatial;
 
 class PathFollower {
-    private PathGenerator pathGen;
-    private BetterCharacterControl spatialControl;
-    private Spatial spatial;
-    private Waypoint currPoint;
-    List<Waypoint> wayPoints = new ArrayList<>();
 
     public PathFollower(Spatial spatial, PathGenerator pathGen) {
         this.spatial = spatial;
-        this.spatialControl = spatial.getControl(BetterCharacterControl.class);
         this.pathGen = pathGen;
     }
 
     public void update() {
-        if (wayPoints.isEmpty()) {
-            Vector3f target = pathGen.getRandomPoint();
-            wayPoints = pathGen.getPath(spatial.getLocalTranslation(), target);
-            try {
-                currPoint = wayPoints.remove(wayPoints.size() - 1);
-            } catch (UnsupportedOperationException e) {
-                System.out.println("caught an exception here");
-                System.out.println("no wayPoints: " + wayPoints.size() + ", target: " + target.getX() + "," + target.getY() + "," + target.getZ());
-                System.exit(1);
-            }
-            System.out.println(", target: " + target.getX() + "," + target.getY() + "," + target.getZ());
-        }
-        if (currPoint == null) {
-            System.out.println("currPoint is null");
-            System.exit(1);
-        }
-        Vector3f currPointVector = currPoint.getPosition();
-        if (spatial.getLocalTranslation().distance(currPointVector) >= 1) {
-            this.spatialControl.setWalkDirection(Vector3f.ZERO);
-            currPoint = wayPoints.remove(wayPoints.size() - 1);
-        } else {
-            Vector3f v = currPointVector.subtract(spatial.getLocalTranslation());
-            spatialControl.setWalkDirection(v.normalize().mult(8));
-            spatialControl.setViewDirection(v.negate());
-        }
     }
 }
 
